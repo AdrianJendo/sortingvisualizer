e6f6dfecfe555628f07268e68a2c4da12f206876
Adrian Jendo <ajendo@uwaterloo.ca>
2021-05-24 14:26:37 -0700

    updates before progress bar

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./SortingVisualizer.css";
 import {mergeSort, quickSort, insertionSort, selectionSort, bubbleSort, heapSort} from '../SortingAlgos/SortingAlgos';
 import {Popup} from './Popup';
-import {Loader} from './Loader';
+import {ProgressBar} from './ProgressBar';
 
 //Animation speed
 //const ANIMATION_SPEED_MS = 0.01; 
@@ -11,7 +11,7 @@ import {Loader} from './Loader';
 const MAX = 1000;
 const MIN = 10;
 //Number of values in array
-const MIN_LEN = 150;
+const MIN_LEN = 15;
 const MAX_LEN = 1500;
 
 //Main colour of array bars
@@ -23,9 +23,7 @@ const SECONDARY_COLOR = 'red';
 //Third colour for pivot, insertion sort, etc.
 const TERTIARY_COLOR = 'green';
 
-//Delay to start animations
-const delay = 3000;
-
+const delay = 2000;
 
 export function SortingVisualizer() {
 
@@ -43,14 +41,25 @@ export function SortingVisualizer() {
     const [displayedElementsColour, setDisplayedElementsColour] = useState(numElementsColour);
     
     const [animationSpeed, setAnimationSpeed] = useState("slow"); //0.01, 0.1, 1, 2?
-    //const [delay, setDelay] = useState(0);
     const button_colour = animationSpeed === "slow" ? "#fa8072" : animationSpeed === "medium" ? "#ffa500" : "#3cb371";
-    const ANIMATION_SPEED_MS = animationSpeed === "slow" ? 1 : animationSpeed === "medium" ? 0.1 : 0.01;
     const [displayedAnimationSpeed, setDisplayedAnimationSpeed] = useState("Slow");
     const [displayedAnimationSpeedColour, setDisplayedAnimationSpeedColour] = useState(button_colour);
-
+    
     const [selectedAlgorithm, setSelectedAlgorithm] = useState("");
     const [doneButtonColour, setDoneButtonColour] = useState("white");
+
+    //const [progress, setProgress] = useState(0);
+
+    //const ANIMATION_SPEED_MS = animationSpeed === "slow" ? 1 : animationSpeed === "medium" ? 0.1 : 0.01;
+
+    const determineAnimationSpeedMS = (algorithm) => {
+        if(algorithm === "merge" || algorithm === "quick" || algorithm === "heap"){
+            return animationSpeed === "slow" ? 1 : animationSpeed === "medium" ? 0.5 : 0.1;
+        }
+        else{
+            return animationSpeed === "slow" ? 0.1 : animationSpeed === "medium" ? 0.05 : 0.01;
+        }
+    }
 
     //Generate array on load
     useEffect( () => {
@@ -73,7 +82,7 @@ export function SortingVisualizer() {
     }
 
     //Change colour of array bar
-    const changeColour = (animations, arrayBars, setColPrimary, i) => {
+    const changeColour = (animations, arrayBars, setColPrimary, i, animation_speed_ms, extra_delay=0) => {
         const [i_barOne, i_barTwo] = animations[i];
         const barOneStyle = arrayBars[i_barOne].style;
         const barTwoStyle = arrayBars[i_barTwo].style;
@@ -81,7 +90,7 @@ export function SortingVisualizer() {
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
-        }, i * ANIMATION_SPEED_MS + delay);
+        }, i * animation_speed_ms + extra_delay + delay);
     }
 
     //change height of array bar
@@ -90,31 +99,31 @@ export function SortingVisualizer() {
         barOneStyle.height = `${newHeight/10}%`
     }
 
-    const swapBars = (animations, arrayBars, i) => {
+    const swapBars = (animations, arrayBars, i, animation_speed_ms, extra_delay=0) => {
         setTimeout( () => {
             const [i_barOne, newHeightOne, i_barTwo, newHeightTwo] = animations[i];
             changeBarHeight(i_barOne, newHeightOne, arrayBars);
             changeBarHeight(i_barTwo, newHeightTwo, arrayBars);
-        }, i* ANIMATION_SPEED_MS + delay);
+        }, i* animation_speed_ms + extra_delay + delay);
     }
 
-    const handleTriColourChange = (animations, arrayBars, i, extra_delay=0) => {
+    const handleTriColourChange = (animations, arrayBars, i, animation_speed_ms, extra_delay=0) => {
         const [i_barOne, colour, set] = animations[i];
         const barOneStyle = arrayBars[i_barOne].style;
         if(colour === 'green'){ //change pivot to green 
             setTimeout(() => { 
                 barOneStyle.backgroundColor = set ? TERTIARY_COLOR : PRIMARY_COLOR; //check if we are setting or resetting bar
-            }, i * ANIMATION_SPEED_MS + delay + extra_delay);
+            }, i * animation_speed_ms + extra_delay + delay);
         }
         else{ //change to red
             setTimeout(() => {
                 barOneStyle.backgroundColor = set ? SECONDARY_COLOR : PRIMARY_COLOR; //check if we are setting or resetting bar
-            }, i * ANIMATION_SPEED_MS + delay + extra_delay);
+            }, i * animation_speed_ms + extra_delay + delay);
         }
     }
 
     //Set array to sorted state
-    const setArraySorted = (sorted, time, extra_delay=0) => {
+    const setArraySorted = (sorted, time, animation_speed_ms, extra_delay=0) => {
         const arrayBars = document.getElementsByClassName('array-bar');
         const final_animation_speed = 2; //ms
 
@@ -122,98 +131,108 @@ export function SortingVisualizer() {
             setTimeout(() => {
                 const barStyle = arrayBars[index].style;
                 barStyle.backgroundColor = TERTIARY_COLOR;
-            }, time*ANIMATION_SPEED_MS + index*final_animation_speed + delay + extra_delay);
+            }, time*animation_speed_ms + index*final_animation_speed + extra_delay + delay);
         });
-
 
         setTimeout( () => { //set state array to sorted array
             setArr(sorted);
             setCurAnimations(false);
             console.log(isSorted(sorted, unsortedArr));
-        }, time*ANIMATION_SPEED_MS + sorted.length*final_animation_speed + delay + extra_delay);
+        }, time*animation_speed_ms + sorted.length*final_animation_speed + extra_delay + delay);
     }
 
     //Merge Sort Function
     const handleMergeSort = () => {
         const animations = [];
+        const start = new Date();
         const sorted = mergeSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("merge");
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("merge");
 
         //animations -- animations is grouped into trios of ([i,j], [i,j], [k, value])
         for(let i = 0; i<animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = i%3 !== 2; //Don't change colour for every third entry (index 2)
+            const cur = new Date() - start;
             if(isColourChange) { //check to change colour
                 const setColPrimary = i % 3 === 0 ? false : true; //Change colour to secondary if first element in trio else primary colour
-                changeColour(animations, arrayBars, setColPrimary, i);
+                changeColour(animations, arrayBars, setColPrimary, i, ANIMATION_SPEED_MS, cur);
             }
             else { //change height
                 setTimeout( () => {
                     const [i_barOne, newHeight] = animations[i];
                     changeBarHeight(i_barOne, newHeight, arrayBars);
-                }, i* ANIMATION_SPEED_MS + delay);
+                }, i* ANIMATION_SPEED_MS + cur + delay);
             }
         }
-        setArraySorted(sorted, animations.length);
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date()-start);
     }
     
     //Quick Sort Function
     const handleQuickSort = () => {
         const animations = [];
+        const start = new Date();
         const sorted = quickSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("quick");
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("quick");
 
         //animations
         let setColPrimary = true; //switch between primary and secondary colour
         for(let i = 0; i<animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = animations[i].length === 2;
+            const cur = new Date() - start;
             if(isColourChange) { //change colour of bar
                 setColPrimary = !setColPrimary;
-                changeColour(animations, arrayBars, setColPrimary, i);
+                changeColour(animations, arrayBars, setColPrimary, i, ANIMATION_SPEED_MS, cur);
             }
             else if(animations[i].length===3){ //set / reset colour of pivot
-                handleTriColourChange(animations, arrayBars, i);
+                handleTriColourChange(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
             else { //swap bars
-                swapBars(animations, arrayBars, i);
+                swapBars(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
         }
-        setArraySorted(sorted, animations.length);
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date() - start);
     }
     
     //Heap Sort Function
     const handleHeapSort = () => {
         const animations = [];
+        const start = new Date();
         const sorted = heapSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("heap");
+
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("heap");
 
         //animations
         let setColPrimary = true; //switch between primary and secondary colour
         for(let i = 0; i<animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = animations[i].length === 2;
+            const cur = new Date() - start;
             if(isColourChange) { //change colour of bar
                 setColPrimary = !setColPrimary;
-                changeColour(animations, arrayBars, setColPrimary, i);
+                changeColour(animations, arrayBars, setColPrimary, i, ANIMATION_SPEED_MS, cur);
             }
             else { //swap bars
-                swapBars(animations, arrayBars, i);
+                swapBars(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
         }
-        setArraySorted(sorted, animations.length);
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date() - start);
     }
     
     //Insertion Sort Function
-    const handleInsertionSort = () => {
+    const handleInsertionSort = async () => {
         const animations = [];
         const start = new Date();
         const sorted = insertionSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("insertion");
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("insertion");
         
         //animations
         for(let i = 0; i<animations.length; i++) {
@@ -221,7 +240,7 @@ export function SortingVisualizer() {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = typeof animations[i][1] === 'string';
             if(isColourChange) { //change colour to green or red
-                handleTriColourChange(animations, arrayBars, i, cur);
+                handleTriColourChange(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
             else {
                 setTimeout( () => {
@@ -238,56 +257,102 @@ export function SortingVisualizer() {
                     }
                     i--;
                     */
-                }, i* ANIMATION_SPEED_MS + delay + cur);
+                }, i* ANIMATION_SPEED_MS + cur + delay);
             }
         }
-        setArraySorted(sorted, animations.length, new Date() - start);
+
+        /*
+        for(let i = 0; i<animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColourChange = typeof animations[i][1] === 'string';
+            
+            new Promise(resolve => {
+                //setTimeout(()=> updateProgress(animations.length, i), 0);
+            }).then(() => {
+                const cur = new Date() - start;
+                if(isColourChange) { //change colour to green or red
+                    handleTriColourChange(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
+                }
+                else {
+                    setTimeout( () => {
+                        for(let j=0; j<animations[i].length-1; j+=2){ //insert pivot in correct location of array and shift over all other elements 
+                            const i_bar = animations[i][j];
+                            const newHeight = animations[i][j+1];
+                            changeBarHeight(i_bar, newHeight, arrayBars + delay);
+                        }
+                        /*
+                        while(i < animations.length && typeof animations[i][1] !== 'string'){ //alternate method to above (more messy so not used)
+                            const [i_bar, newHeight] = animations[i];
+                            changeBarHeight(i_bar, newHeight, arrayBars);
+                            i++;
+                        }
+                        i--;
+                        /
+                    }, i* ANIMATION_SPEED_MS + cur);
+                }
+            });
+        }
+        */
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date() - start);
+    }
+
+    const updateProgress = (len, i) => {
+        const value = Math.round(i/len*100);
+        const progressBar = document.querySelector(".progress");
+        progressBar.querySelector(".progress__fill").style.width = `${value}%`;
+        progressBar.querySelector(".progress__text").textContent = `${value}%`;
     }
 
     //Selection Sort Function
     const handleSelectionSort = () => {
         const animations = [];
+        const start = new Date();
         const sorted = selectionSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("selection");
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("selection");
 
         //animations
         for(let i = 0; i<animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = animations[i].length === 3;
+            const cur = new Date() - start;
             if(isColourChange) { //change colour to green or red
-                handleTriColourChange(animations, arrayBars, i);
+                handleTriColourChange(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
             else {
-                swapBars(animations, arrayBars, i);
+                swapBars(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
         }
 
-        setArraySorted(sorted, animations.length);
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date() - start);
     }
 
     //Bubble Sort Function
     const handleBubbleSort = () => {
         const animations = [];
+        const start = new Date();
         const sorted = bubbleSort(arr, animations, ascending);
         setCurAnimations(true);
         setSelectedAlgorithm("bubble");
+        const ANIMATION_SPEED_MS = determineAnimationSpeedMS("bubble");
 
         //animations
         let setColPrimary = true; //switch between primary and secondary colour
         for(let i = 0; i<animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColourChange = animations[i].length === 2;
+            const cur = new Date() - start;
             if(isColourChange) { //change colour of bar
                 setColPrimary = !setColPrimary;
-                changeColour(animations, arrayBars, setColPrimary, i);
+                changeColour(animations, arrayBars, setColPrimary, i, ANIMATION_SPEED_MS, cur);
             }
             else { //swap bars
-                swapBars(animations, arrayBars, i);
+                swapBars(animations, arrayBars, i, ANIMATION_SPEED_MS, cur);
             }
         }
 
-        setArraySorted(sorted, animations.length);
+        setArraySorted(sorted, animations.length, ANIMATION_SPEED_MS, new Date() - start);
     }
 
     //Tests sorting algorithms
@@ -330,7 +395,6 @@ export function SortingVisualizer() {
 
     //Checks if individual array is sorted correcty
     const isSorted = (sorted, unsorted) => {
-
         const js_sorted = ascending ? unsorted.slice().sort((a,b) => a - b) : unsorted.slice().sort((a,b) => b - a); //sort using js sorting function
         if (js_sorted.length !== sorted.length) return false;
             for(let i = 0; i < js_sorted.length; i++){
@@ -338,7 +402,6 @@ export function SortingVisualizer() {
                     return false;
             }
         }
-
         return true;
     }
 
@@ -415,28 +478,6 @@ export function SortingVisualizer() {
             alert("Testing Failed");
         }
     }
-
-    /*
-    useEffect(() => {
-        switch(selectedAlgorithm) {
-            case "insertion":
-                handleInsertionSort();
-                break;
-            default:
-                break;
-        }
-    }, [selectedAlgorithm]);
-
-
-    useEffect(() => {
-        if(curAnimation && !disableLoading){
-            setLoading(true);
-        }
-        else{
-            setLoading(false);
-        }
-    }, [curAnimation, disableLoading]);
-    */
 
     const buttonsClickable = curAnimation || unsortedArr !== arr ? "unclickable" : "";
     const resetControls = curAnimation ? "unclickable" : "";
@@ -561,12 +602,21 @@ export function SortingVisualizer() {
                             <div className="popup-row">
                                 <div className="popup-column column-double">
                                     <div className="slidecontainer">
-                                        <input type="range" min="150" max="1500" value={numElements} className="slider" id="myRange" onChange={(e) => updateNumElements(e)}/>
+                                        <input type="range" min="15" max="1500" value={numElements} className="slider" id="myRange" onChange={(e) => updateNumElements(e)}/>
                                     </div>
                                 </div>
                                 <div className="popup-column column-double">
-                                    {/*<b1>Value: <span style={{color:"red"}}>{numElements}</span></b1>*/}
-                                    <span>Value: <input className="numElements-input" type="number" min="15" max="1500" style={{color:numElementsColour}} value={numElements} onChange={(e)=>updateNumElements(e)}/></span>
+                                    <span>
+                                        Value: 
+                                        <input  className="numElements-input" 
+                                                type="number" 
+                                                min="15" 
+                                                max="1500" 
+                                                style={{color:numElementsColour}} 
+                                                value={numElements} 
+                                                onChange={(e)=>updateNumElements(e)}
+                                        />
+                                    </span>
                                 </div>
                             </div>
                             <div className="popup-row">
@@ -589,7 +639,7 @@ export function SortingVisualizer() {
                             </div>
                     </div>}
             />}
-            {/*loading && <Loader/>*/}
+            {/*progress >= 0 && progress < 1000 && <ProgressBar progress={progress}/>*/}
         </div>
     );
 }
